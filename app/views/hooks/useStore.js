b8f1bcd8f97e2fa86974e5f9a2946a2795538ccd
Joseph Clay <joe@weareimmutable.com>
2020-07-10 13:12:04 -0500

    remove unused hook

--- app/views/hooks/useStore.js
+++
@@ -1,7 +0,0 @@
-import { useStore as useStoreStore } from '@immutabl3/store/react';
-
-// TODO: kill this useStore
-
-export default function useStore(selector) {
-  return useStoreStore(selector);
-};
